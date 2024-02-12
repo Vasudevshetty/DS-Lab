7a84b009688246d12e668ab52b0d2f147310d5ad
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-12 22:55:00 +0530

    Completed the desing-2 implmentation of static priority queue (descending type) with enqueue manipulated queue, (note down the conditions in enqueue functions.)

--- Queue_session/priority_queue/staticPriorityQueue/queueDesign2.c
+++ Queue_session/priority_queue/staticPriorityQueue/queueDesign2.c
@@ -16,9 +16,11 @@ typedef struct
     int priority;
 } Element;
 
-Element* createElement(int data, int priority){
+Element *createElement(int data, int priority)
+{
     Element *element = (Element *)malloc(sizeof(Element));
-    if(!element){
+    if (!element)
+    {
         printf("Memmory allocation failed.\n");
         exit(1);
     }
@@ -64,10 +66,10 @@ priorityQueue *createPriorityQueue(int sz)
     return que;
 }
 
-//support functions.
+// support functions.
 bool isEmpty(priorityQueue *que)
 {
-    return front == -1;
+    return front == -1 && rear == -1;
 }
 
 bool isFull(priorityQueue *que)
@@ -75,51 +77,78 @@ bool isFull(priorityQueue *que)
     return rear == size - 1;
 }
 
-/*The functions dequeue, enqueue, peek front and rear accepts and returns the pointer of the element, 
-so unnessacary copying will be avoided. This is the significant way of achieving it. The element created in 
-the respective activation frames are passed as a reference avoiding copying of it and the element can be accessed in 
+/*The functions dequeue, enqueue, peek front and rear accepts and returns the pointer of the element,
+so unnessacary copying will be avoided. This is the significant way of achieving it. The element created in
+the respective activation frames are passed as a reference avoiding copying of it and the element can be accessed in
 the respective functions.*/
 
-void enqueue(priorityQueue *que, Element* element)
+void enqueue(priorityQueue *que, Element *element)
 {
     if (isFull(que))
     {
         printf("Queue overflow, enqueuing failed.\n");
         return;
     }
-    
+    if (isEmpty(que))
+    {
+        front = rear = 0;
+        ele[rear] = *element;
+        return;
+    }
+    else
+        rear++;
+    int idx = rear;
+    // since this a descending queue, the lowest prioirty task wil be enqueued at front.
+    while (idx > front && ele[idx - 1].priority < element->priority)
+    {
+        ele[idx] = ele[idx - 1];
+        idx--;
+    }
+    ele[idx] = *element;
 }
 
-Element* dequeue(priorityQueue *que)
+Element *dequeue(priorityQueue *que)
 {
     if (isEmpty(que))
     {
         printf("Queue underflow, dequeuing failed.\n");
         return NULL;
     }
+    Element *element = &ele[front];
+    if (front == rear)
+        front = rear = -1;
+    else
+        front++;
+    return element;
 }
 
-Element* peekFront(priorityQueue* que){
+Element *peekFront(priorityQueue *que)
+{
     return isEmpty(que) ? NULL : &ele[front];
 }
 
-Element* peekRear(priorityQueue* que){
+Element *peekRear(priorityQueue *que)
+{
     return isEmpty(que) ? NULL : &ele[rear];
 }
 
-void display(priorityQueue* que){
-    if(isEmpty(que)){
+void display(priorityQueue *que)
+{
+    if (isEmpty(que))
+    {
         printf("Queue under flow, no elements to display.\n");
         return;
     }
     printf("Elements of the queue are, \n");
-    for (int i = front; i <= rear; i++){
+    for (int i = front; i <= rear; i++)
+    {
         printf("Element => data : %d\tpriority : %d\t\n", ele[i].data, ele[i].priority);
     }
 }
 
-//drive code.
-int main(){
+// drive code.
+int main()
+{
     int sz;
     printf("Enter size of the queue : ");
     scanf("%d", &sz);
@@ -127,40 +156,44 @@ int main(){
     int data, priority;
     int choice;
 
-    do{
+    do
+    {
         printf("Menu.\n1. Enqueue.\n2. Dequeue.\n3. Front.\n4. Rear\n5. Display\n6. Exit\n\n");
         printf("Enter choice : ");
         scanf("%d", &choice);
         Element *element;
-        switch(choice){
-            case 1:
-                printf("Enter data and priority : ");
-                scanf("%d %d", &data, &priority);
-                element = createElement(data, priority);
-                enqueue(que, element);
-                display(que);
-                break;
-            case 2:
-                element = dequeue(que);
-                printf("The dequeued element's data is %d and priority is %d\n", element->data, element->priority);
-                display(que);
-                break;
-            case 3: 
-                element = peekFront(que);
-                printf("The front element's data is %d and priority is %d\n", element->data, element->priority);
-                break;
-            case 4: 
-                element = peekRear(que);
-                printf("The rear element's data is %d and priority is %d\n", element-data, element->priority);
-                break;
-            case 5 :
-                display(que);
-                break;
-            case 6:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Enter valid choice only, please try again.\n");
+        switch (choice)
+        {
+        case 1:
+            printf("Enter data and priority : ");
+            scanf("%d %d", &data, &priority);
+            element = createElement(data, priority);
+            enqueue(que, element);
+            display(que);
+            break;
+        case 2:
+            element = dequeue(que);
+            if(element) printf("The dequeued element's data is %d and priority is %d\n", element->data, element->priority);
+            display(que);
+            break;
+        case 3:
+            element = peekFront(que);
+            !element ? printf("Queue underflow, no elements to peek.\n")
+             : printf("The front element's data is %d and priority is %d\n", element->data, element->priority);
+            break;
+        case 4:
+            element = peekRear(que);
+            !element ? printf("Queue underflow, no elements to peek.\n")
+             : printf("The rear element's data is %d and priority is %d\n", element->data, element->priority);
+            break;
+        case 5:
+            display(que);
+            break;
+        case 6:
+            printf("Exiting...\n");
+            break;
+        default:
+            printf("Enter valid choice only, please try again.\n");
         }
         printf("\n\n");
     } while (choice != 6);
