b9c7503f5ae6cdf81b3b58b213a8682e5036387d
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-19 15:01:46 +0530

    Reordreed the stack repo

---
+++ Stack_session/Notation-Interconvention/infixPostfixPrefix.c
@@ -0,0 +1,224 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct Node
+{
+    char element;
+    struct Node *next;
+} node;
+
+typedef struct
+{
+    struct Node *Top;
+    int Count;
+} Stack;
+
+#define top stack->Top
+#define count stack->Count
+
+node *createNode(char element)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    if (!newNode)
+    {
+        printf("Memory allocation failed.\n");
+        printf("Stack overflow.\n");
+        exit(1);
+    }
+    newNode->element = element;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Stack *createStack()
+{
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    if (!stack)
+    {
+        printf("Memory allocaitn failad.\n");
+        exit(1);
+    }
+    top = NULL;
+    count = 0;
+    return stack;
+}
+
+bool isEmpty(Stack *stack)
+{
+    return !(top && count);
+}
+
+void display(Stack *stack)
+{
+    if (!isEmpty(stack))
+    {
+        node *temp = top;
+        while (temp)
+        {
+            node *next = temp->next;
+            free(temp);
+            temp = next;
+        }
+    }
+}
+
+void push(Stack *stack, char element)
+{
+    node *newNode = createNode(element);
+    if (isEmpty(stack))
+    {
+        top = newNode;
+    }
+    else
+    {
+        newNode->next = top;
+        top = newNode;
+    }
+    count++;
+}
+
+char pop(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        // printf("Empty stack to pop. (Stack underflow.)\n");
+        return -1;
+    }
+    node *toDelete = top;
+    int element = toDelete->element;
+    top = top->next;
+    count--;
+    free(toDelete);
+    return element;
+}
+
+char stackTop(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        // printf("Stack underflow, no elements at the stack top.\n");
+        return -1;
+    }
+    return top->element;
+}
+
+int precedence(char c)
+{
+    if (c == '+' || c == '-')
+        return 1;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '^')
+        return 3;
+    else
+        return 0;
+}
+
+bool isOperand(char c)
+{
+    return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
+
+char *infixToPostfix(const char *infix)
+{
+    int i = 0, j = 0;
+    Stack *stack = createStack();
+    char *postfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    while (infix[i] != '\0')
+    {
+        char token = infix[i];
+        if (isOperand(token))
+        {
+            postfix[j++] = token;
+        }
+        else if (token == '(')
+            push(stack, token);
+        else if (token == ')')
+        {
+            while (!isEmpty(stack) && stackTop(stack) != '(')
+                postfix[j++] = pop(stack);
+            if (!isEmpty(stack) && stackTop(stack) == '(')
+                pop(stack);
+        }
+        else
+        {
+            while ((!isEmpty(stack) && precedence(token) <= precedence(stackTop(stack))) || (precedence(token) == precedence(stackTop(stack)) && stackTop(stack) == '^'))
+                postfix[j++] = pop(stack);
+            push(stack, token);
+        }
+        i++;
+    }
+    while (!isEmpty(stack))
+        postfix[j++] = pop(stack);
+    postfix[j] = '\0';
+    return postfix;
+}
+
+char *infixToPrefix(const char *infix)
+{
+    char *reversedInfix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    int k = 0;
+    for (int i = strlen(infix) - 1; i >= 0; i--)
+        reversedInfix[k++] = infix[i];
+    reversedInfix[k] = '\0';
+
+    char *prefix = (char *)malloc((strlen(infix) + 1) * sizeof(char));
+    int i = 0, j = 0;
+    Stack *stack = createStack();
+    while (reversedInfix[i] != '\0')
+    {
+        char token = reversedInfix[i];
+        if (isOperand(token))
+            prefix[j++] = token;
+        else if (token == ')')
+            push(stack, token);
+        else if (token == '(')
+        {
+            while (!isEmpty(stack) && stackTop(stack) != ')')
+                prefix[j++] = pop(stack);
+            if (!isEmpty(stack) && stackTop(stack) == ')')
+                pop(stack);
+        }
+        else
+        {
+            while (!isEmpty(stack) && precedence(token) < precedence(stackTop(stack)))
+                prefix[j++] = pop(stack);
+            push(stack, token);
+        }
+        i++;
+    }
+    while (!isEmpty(stack))
+        prefix[j++] = pop(stack);
+
+    prefix[j] = '\0';
+
+    for (int i = 0, k = strlen(prefix) - 1; i < k; i++, k--)
+    {
+        char temp = prefix[i];
+        prefix[i] = prefix[k];
+        prefix[k] = temp;
+    }
+
+    return prefix;
+}
+
+bool isDigit(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+bool isOperator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^';
+}
+
+int main()
+{
+    char infix[100];
+    scanf("%s", infix);
+    printf("%s, %s", infixToPostfix(infix), infixToPrefix(infix));
+    printf("\n");
+    return 0;
+}
---
+++ Stack_session/infix, prefix, postfix final/evaluatePostPrefix.c
@@ -0,0 +1,165 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+// enum to find out the notation.
+typedef enum Notation
+{
+    prefix,
+    postfix,
+} Notation;
+
+typedef struct Node
+{
+    int data;
+    struct Node *next;
+} node;
+
+typedef struct
+{
+    node *Top;
+    int Count;
+} Stack;
+
+#define top stack->Top
+#define count stack->Count
+
+node *createNode(int data)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    if (!newNode)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Stack *initStack()
+{
+    Stack *stack = (Stack *)malloc(sizeof(Stack));
+    if (!stack)
+    {
+        printf("Memory allcation failed.\n");
+        exit(1);
+    }
+    top = NULL;
+    count = 0;
+    return stack;
+}
+
+bool isEmpty(Stack *stack)
+{
+    return !(top && count);
+}
+
+void push(Stack *stack, int data)
+{
+    node *newNode = createNode(data);
+    if (isEmpty(stack))
+        top = newNode;
+    else
+    {
+        newNode->next = top;
+        top = newNode;
+    }
+    count++;
+}
+
+int pop(Stack *stack)
+{
+    if (isEmpty(stack))
+    {
+        printf("Stack empty to pop.\n");
+        return -1;
+    }
+    node *toDelete = top;
+    int data = toDelete->data;
+    top = toDelete->next;
+    count--;
+    return data;
+}
+
+int peek(Stack *stack)
+{
+    return isEmpty(stack) ? -1 : top->data;
+}
+// the above is the stack implementation using linkedlist.
+
+// the support fnction to calcuate the value of teh oeprations of operands speciefed,
+int calculate(char operator, int operand1, int operand2)
+{
+    switch (operator)
+    {
+    case '+':
+        return operand1 + operand2;
+    case '-':
+        return operand1 - operand2;
+    case '*':
+        return operand1 * operand2;
+    case '/':
+        return operand1 / operand2;
+    }
+    return -1;
+}
+
+bool isOperator(char c)
+{
+    return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+// the function to evalute the expression based on the noatation the expression is followed, used dry principle to finish everything
+// in a single function.
+int evualate(const char *expression, Notation notation)
+{
+    Stack *stack = initStack();
+    int i = 0, len = strlen(expression);
+    while(i < len){
+        char token = notation == prefix ? expression[len - i - 1] : expression[i];
+        if(isalnum(token))
+            push(stack, token - '0');
+        else if(isOperator(token)){
+            int operand1 = pop(stack);
+            int operand2 = pop(stack);
+
+            int result = notation == prefix ? calculate(token, operand1, operand2) : calculate(token, operand2, operand1);
+            push(stack, result);
+        }
+        i++;
+    }
+    return pop(stack);
+}
+
+// driver code.
+int main()
+{
+    char expression[100];
+    int choice, result;
+    char notation[10];
+    printf("Enter the expression to calcualte : ");
+    scanf("%s", expression);
+    printf("Please enter the type of notation used in the expression : ");
+    scanf("%s", notation);
+    if(strcmp(notation, "prefix") == 0)
+        result = evualate(expression, prefix);
+    else if(strcmp(notation, "postfix") == 0)
+        result = evualate(expression, postfix);
+    else {
+        printf("Please specify the correct type of notation (prefix or postfix).\n");
+        main();
+    }
+    printf("The calcualted result of the expression is %d\n", result);
+    printf("\n1. Try again\t 2. Exit.\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+    printf("\n\n");
+    if(choice == 1)
+        main();
+    else
+        printf("Exiting....\n");
+    return 0;
+}
