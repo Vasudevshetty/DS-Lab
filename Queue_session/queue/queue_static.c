471a6260e164ddddb65e579ad32f7d3a6812fd36
Vasudevshetty <vasudeepu2815@gmail.com>
2024-02-29 18:06:01 +0530

    Added final refactor the code, added few good practices like, which those cannot modify the state of the data that has been sent to serveral functions, and to improvise performance for few single line functions made them as inline functions. Final comments check up and the things will be done before i complete this repositery's work by this month end.

--- Queue_session/queue/queue_static.c
+++ Queue_session/queue/queue_static.c
@@ -9,54 +9,32 @@ mechanisms using array storage representation.(Represent Queue using structure)*
 // structure to store queue members.
 struct queue
 {
-    int *container; // array to store the elements.
-    int front;      // pointer to manipulate from the front of the queue.
-    int rear;       // pointer to manipulate from the rear of the queue.
-    int size;       // variable to store the size of the container.
+    int container[max_size]; // array to store the elements.
+    int front;               // pointer to manipulate from the front of the queue.
+    int rear;                // pointer to manipulate from the rear of the queue.
+    int size;                // variable to store the size of the container.
 };
 
 // functions to allcoate memeory.
-void createStaticQueue(struct queue *q);
-void createDynamicQueue(struct queue *q, int size);
+struct queue *createStaticQueue();
 
 // condtions.
-int isEmpty(struct queue q);
-int isFull(struct queue q);
+inline int isEmpty(const struct queue *);
+inline int isFull(const struct queue *);
 
 // operations.
-void enqueue(struct queue *q, int data);
-int dequeue(struct queue *q);
-int front(struct queue q);
-int rear(struct queue q);
+void enqueue(struct queue *, int);
+int dequeue(struct queue *);
+inline int front(const struct queue *);
+inline int rear(const struct queue *);
 
 // display function
-void display(struct queue q);
+void display(const struct queue *);
 
 int main()
 {
-    int data, choice, size;
-    struct queue q;
-    q.container = NULL;
-
-    printf("Enter mode of allocation.\n1. Static\n2. Dynamic\n");
-    printf("Enter the choice: ");
-    scanf("%d", &choice);
-
-    if (choice == 1)
-    {
-        createStaticQueue(&q);
-    }
-    else if (choice == 2)
-    {
-        printf("Enter the size of the queue: ");
-        scanf("%d", &size);
-        createDynamicQueue(&q, size);
-    }
-    else
-    {
-        printf("Invalid choice. Retry...\n");
-        main();
-    }
+    int data, choice;
+    struct queue *q = createStaticQueue();
 
     do
     {
@@ -76,20 +54,22 @@ int main()
         case 1:
             printf("Enter the data to enqueue: ");
             scanf("%d", &data);
-            enqueue(&q, data);
+            enqueue(q, data);
+            display(q);
             break;
         case 2:
-            data = dequeue(&q);
+            data = dequeue(q);
             if (data != -1)
                 printf("The dequeued data is %d\n", data);
+            display(q);
             break;
         case 3:
-            if(front(q) != -1)
-            printf("The front of the data is %d\n", front(q));
+            if (front(q) != -1)
+                printf("The front of the data is %d\n", front(q));
             break;
         case 4:
-            if(rear(q) != -1)
-            printf("The rear of the data is %d\n", rear(q));
+            if (rear(q) != -1)
+                printf("The rear of the data is %d\n", rear(q));
             break;
         case 5:
             display(q);
@@ -103,50 +83,35 @@ int main()
         }
     } while (choice != 6);
 
-    free(q.container);
     return 0;
 }
 
 // function to create static memory allcoated queue.
 /*The memory is allocateed at the compile time itslef though it is allocated at heap.*/
-void createStaticQueue(struct queue *q)
+struct queue *createStaticQueue()
 {
-    q->container = (int *)malloc(max_size * sizeof(int));
-    if (!q->container)
+    struct queue *q = (struct queue *)malloc(sizeof(struct queue));
+    if (!q)
     {
-        printf("Memory allcoation failed.\n");
-        return;
+        printf("Memory allcoatin failed.\n");
+        exit(1);
     }
     q->front = -1;
     q->rear = -1;
     q->size = max_size;
-}
-
-// function to create dynamic memory allocated queue.
-/*Since dynamic memory for the array is allocated at run time upon reciveing input from the user.*/
-void createDynamicQueue(struct queue *q, int size)
-{
-    q->container = (int *)malloc(size * sizeof(int));
-    if (!q->container)
-    {
-        printf("Memory allocation failed.\n");
-        return;
-    }
-    q->front = -1;
-    q->rear = -1;
-    q->size = size;
+    return q;
 }
 
 // function to check whether the queue is empty.
-int isEmpty(struct queue q)
+inline int isEmpty(const struct queue *q)
 {
-    return q.front == -1 && q.rear == -1;
+    return q->front == -1 && q->rear == -1;
 }
 
 // function to check whetehr the queue is full.
-int isFull(struct queue q)
+inline int isFull(const struct queue *q)
 {
-    return q.rear == q.size - 1;
+    return q->rear == q->size - 1;
 }
 
 // operations
@@ -154,12 +119,12 @@ int isFull(struct queue q)
 void enqueue(struct queue *q, int data)
 {
     // check whether the queue is full, if full cannot enqueue
-    if (isFull(*q))
+    if (isFull(q))
     {
         printf("Queue is full, enqueue failed.\n");
         return;
     }
-    else if (isEmpty(*q))
+    else if (isEmpty(q))
     {
         // if suppose it is the first element to enqueue intailise front
         q->front = 0;
@@ -175,7 +140,7 @@ void enqueue(struct queue *q, int data)
 int dequeue(struct queue *q)
 {
     // check whether the queue is empty.
-    if (isEmpty(*q))
+    if (isEmpty(q))
     {
         printf("Queue is empty, dequeue failed.\n");
         return -1;
@@ -201,7 +166,7 @@ int dequeue(struct queue *q)
 }
 
 // support function returning front and rear values
-int front(struct queue q)
+inline int front(const struct queue *q)
 {
     // check whether the queue is empty before accessing.
     if (isEmpty(q))
@@ -210,10 +175,10 @@ int front(struct queue q)
         return -1;
     }
 
-    return q.container[q.front];
+    return q->container[q->front];
 }
 
-int rear(struct queue q)
+inline int rear(const struct queue *q)
 {
     // check whether the queue is empty before accessing.
     if (isEmpty(q))
@@ -222,20 +187,23 @@ int rear(struct queue q)
         return -1;
     }
 
-    return q.container[q.rear];
+    return q->container[q->rear];
 }
 
 // function to display the queue contenets.
-void display(struct queue q){
+void display(const struct queue *q)
+{
     // check whetehr if it is empty.
-    if(isEmpty(q)){
+    if (isEmpty(q))
+    {
         printf("Queue is empty, no elements to display.\n");
         return;
     }
 
     printf("Queue elements : ");
-    for(int i = q.front; i <= q.rear; i++){
-        printf("%d ", q.container[i]);
+    for (int i = q->front; i <= q->rear; i++)
+    {
+        printf("%d ", q->container[i]);
     }
     printf("\n");
 }
