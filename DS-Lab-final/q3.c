c772f788adc5bce6280d04fa63a64c3623e04955
Vasudevshetty <vasudeepu2815@gmail.com>
2024-03-05 18:55:20 +0530

    Aborted the plan of completing question again. Should focus on my dsa straek, strivers dsa sheet.

--- DS-Lab-final/q3.c
+++
@@ -1,3 +0,0 @@
-/*Develop a menu driven program to implement the following operations on an array of
-integers with dynamic memory allocation. Display the array contents after each operation.
-i) Insert by order ii) Delete by position iii) Search by key iv) Reverse the contents*/
--- DS-Lab-final/q5.c
+++
@@ -1,3 +0,0 @@
-/*Implement circular double linked list to perform the following operations
- i) Insert by order ii ) Delete by position iii ) Delete by key iv) Search by position
- Display the list contents after each operation*/
