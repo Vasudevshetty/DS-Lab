7720a745fc8028ca99d6928ec28930debd5bec2f
Vasudevshetty <vasudeepu2815@gmail.com>
2024-03-12 19:57:56 +0530

    Updated the student program too .

--- struct_session/student_marks.c
+++ struct_session/student_marks.c
@@ -5,147 +5,107 @@ Note: Illustrate the use of pointer to an array of structure and allocate memory
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // structure for student
-struct student
+typedef struct Student
 {
     char name[30];
     int reg_no;
     int marks[3];
     float average_marks;
-};
+} Student;
 
-// function for reading information of N students
-void readStudentInfo(struct student *students, int N)
-{ // students is sent as a pointer to a array of structures.
-    for (int i = 0; i < N; i++)
+float calculateAverageMarks(const int marks[])
+{
+    int sum = 0, min = marks[0];
+    for (int i = 0; i < 3; i++)
     {
-        printf("Enter details of student %d,\n", i + 1);
-        printf("Enter name : ");
-        scanf("%s", (students+i)->name);
-        printf("Enter Registration number : ");
-        scanf("%d", &(students+i)->reg_no);
-        printf("Marks in 3 tests: ");
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &(students+i)->marks[j]);
-        }
-        printf("\n");
+        sum += marks[i];
+        min = marks[i] < min ? marks[i] : min;
     }
+    return (sum - min) / 2.0;
 }
 
-// function for calculating average marks , best of two in the marks of the students.
-void calculateAverageMarks(struct student *students, int N)
+Student *initStudent(const char *name, int reg_no, int marks[])
 {
-    for (int i = 0; i < N; i++)
-    {  
-        int min = ((students+i)->marks[0] < (students+i)->marks[1]) ? ((students+i)->marks[0] < (students+i)->marks[2]) ? 
-        (students+i)->marks[0] : (students+i)->marks[2] : ((students+i)->marks[1] < (students+i)->marks[2]) ? (students+i)->marks[1] :
-        (students+i)->marks[2];
-        // calculate the average of best two marks and assign to the structure.
-        // The array is sorted so the last few elements are the best.
-        (students+i)->average_marks =  (float)((students+i)->marks[0] + (students+i)->marks[1] + (students+i)->marks[2] - min)/ 2.0;
-        // type cast it & index are (n-2) and (n-1) respectively. (n = 3) marks array.;
+    Student *student = (Student *)malloc(sizeof(Student));
+    if (!student)
+    {
+        printf("Memory allocation failed.\n");
+        exit(1);
     }
+    strncpy(student->name, name, sizeof(student->name));
+    student->name[sizeof(name)] = '\0';
+    student->reg_no = reg_no;
+    for (int i = 0; i < 3; i++)
+        student->marks[i] = marks[i];
+    student->average_marks = calculateAverageMarks(marks);
+    return student;
+}
+
+void displayInfo(const Student *student)
+{
+    printf("\nThe information of the student are,\nName : %s\nRegistration no : %d\nMarks : %d %d %d\nAverage Marks : %.2f\n\n",
+           student->name, student->reg_no, student->marks[0], student->marks[1], student->marks[2], student->average_marks);
+}
+
+void readStudent(Student *students, int i)
+{
+    printf("\nEnter the details of the student, \n");
+    char name[24];
+    int reg_no, marks[3];
+    printf("Enter name : ");
+    scanf("%s", name);
+    printf("Enter reg no: ");
+    scanf("%d", &reg_no);
+    printf("Enter marks : ");
+    scanf("%d %d %d", &marks[0], &marks[1], &marks[2]);
+    students[i] = *initStudent(name, reg_no, marks);
+    printf("\n");
 }
 
 // function for displaying information of N students
-void displayStudentInfo(struct student *students, int N)
-{
-    int anyStudents = 0; // Check if information is entered or not.
-    int calculated = 0;  // Check if average marks is calculated or not.
-
-    for (int i = 0; i < N; i++)
-    {
-        if ((students+i)->reg_no == 0)
-            anyStudents = 1;
-        if ((students+i)->average_marks == 0.0)
-            calculated = 1;
-    }
-
-    if (anyStudents)
-    {
-        printf("No Students entered. Please select option 1 to enter student information\n");
-        return;
-    }
-    else if (calculated)
-    {
-        printf("Average marks not calculated. Please select option 2 to calculate and select option 3 later.\n");
-        return;
-    }
-
-    printf("Student Information:\n\n");
-    for (int i = 0; i < N; i++)
-    {
-        printf("Student %d\n", i + 1);
-        printf("Name : %s\n", (students+i)->name);
-        printf("Registration number: %d\n", (students+i)->reg_no);
-        printf("Marks in 3 tests are : ");
-        for (int j = 0; j < 3; j++)
-            printf("%d, ", (students+i)->marks[j]);
-        printf("\nAverage marks (best of two): %.2f\n", (students+i)->average_marks);
-        printf("\n");
-    }
-}
-
 int main()
 {
     int N;
     printf("Enter number of students: ");
     scanf("%d", &N);
 
-    if (N == 0)
+    Student *students = (Student *)malloc(sizeof(Student) * N);
+    if (!students)
     {
-        printf("No students..\nEnter a valid number: ");
-        scanf("%d", &N);
+        printf("Memory allocatin failed.\n");
+        exit(1);
     }
 
-    /*We use calloc since upon dynamically creating a array of structures using malloc will create a array of structures with garbage value
-    so as to initalise with 0(or respecive acc. to its type) can be done through calloc.*/
-    struct student *students = (struct student *)calloc(N, sizeof(struct student));
-    // struct student *students = (struct student *)malloc(N * sizeof(struct student)); (We can also use malloc, but cases cannot be handled.)
-
-    if (students == NULL)
-    {
-        printf("Memory allocation failed\n Exiting.....");
-        return 1;
-    }
     int choice;
     do
     {
-        printf("\n\n");
-        printf("Menu\n");
+        printf("\nMenu\n");
         printf("1. Read Student Information\n");
-        printf("2. Calculate Average Marks\n");
-        printf("3. Display Student Information\n");
-        printf("4. Exit\n");
+        printf("2. Display Student Information\n");
+        printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        printf("\n\n\n");
-        switch (choice)
+        for (int i = 0; i < N && choice != 3; i++)
         {
-        case 1:
-            readStudentInfo(students, N);
-            break;
-        case 2:
-            calculateAverageMarks(students, N);
-            break;
-        case 3:
-            displayStudentInfo(students, N);
-            break;
-        case 4:
-            printf("Exiting...\n");
-            break;
-        default:
-            printf("Invalid Choice... Try again.\n");
-            break;
+            switch (choice)
+            {
+            case 1:
+                readStudent(students, i);
+                break;
+            case 2:
+                displayInfo(students + i);
+                break;
+            case 3:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid Choice... Try again.\n");
+                break;
+            }
         }
-        printf("\n\n");
-    } while (choice != 4);
-    free(students);
+    } while (choice != 3);
     return 0;
 }
-
-// Please format the way you want, i.e the indentation on the terminal..
-/*handled 2 cases, if not calculating average marks and not entering the information,
-where one tries to display by printing appropriate messages.*/
