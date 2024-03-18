8926e2bd1d00f339ff10dbbbae4bd71d3cd906f4
Vasudevshetty <vasudeepu2815@gmail.com>
2024-03-18 08:38:00 +0530

    Started completeing the ds lab questions by soving graph questions, finished with the structure type and its creating functoin.

--- graphs_session/graphADT.c
+++ graphs_session/graphADT.c
@@ -1,1 +1,34 @@
 /*Develop a menu driven program to implement Graph traversal techniques*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Graph
+{
+    int **adjList;
+    int vertex;
+} Graph;
+
+Graph *createGraph(int vertex)
+{
+    Graph *graph = (Graph *)malloc(sizeof(Graph));
+    if (!graph)
+    {
+        printf("Memroy allocation failed.\n");
+        exit(1);
+    }
+    graph->vertex = vertex;
+    graph->adjList = (int **)malloc(sizeof(int *) * graph->vertex);
+
+    if(!graph->adjList){
+        free(graph);
+        printf("Memroy allocation failed.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < graph->vertex; i++)
+        graph->adjList[i] = NULL;
+
+    return graph;
+}
+
